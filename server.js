8286bf980c171497071137fa26d28b89d0537ced
Rory <rory88@gmail.com>
2016-02-03 14:15:39 +0000

    early stages

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
 var express = require('express');
 var app = express();
+var moment = require('moment')
 
 app.get('/', (req, res) => {
   	res.send('Hello World and shitd');
 });
 
 app.get(/./g, (req, res) => {
-	console.log('requested something: ' + req.url)
-	res.send({unixTimestamp: 'hello', naturalLanguageDate: 'welt'})
+
+	const inputIsUnix = url => {
+		return !(/[a-zA-Z]/g.test(url))
+	};
+
+	const inputIsDate = url => {
+		return false;
+	}
+
+	const url = req.url.substr(1);
+	console.log('requested something: ' + url)
+
+	var unix = 0,
+		natural = '';
+
+	if (inputIsUnix(url)) {	
+		unix = Number(url);
+		natural = moment(moment.unix(url)._d).format('MMMM D, YYYY')
+	} 
+	else if (inputIsDate(url)) {
+		
+		console.log('NOT VALID UNIX TIMESTAMP!')
+	} 
+	else {
+		console.log('NOT VALID UNIX TIMESTAMP!')
+		console.log('here:', moment(url, 'MMMM D, YYYY').toDate());
+		unix = null;
+		natural = null;
+	}
+
+	res.send({'unixTimestamp': unix, 'naturalLanguageDate': natural})
 });
 
 var port = Number(process.env.PORT || 3000);
